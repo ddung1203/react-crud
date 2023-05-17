57922f09e78cd8d39f3a51ef338ef926ea976fb8
Joongseok Jeon <jeonjungseok1203@gmail.com>
2023-05-17 04:28:42 +0000

    [ADD] Products CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 
 import Products from "./admin/Products"
-import Main from "./main/Main"
 import { BrowserRouter, Route } from "react-router-dom";
+import Main from "./main/Main"
+import ProductsCreate from "./admin/ProductsCreate";
+import ProductsEdit from "./admin/ProductsEdit";
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
 
             <BrowserRouter>
               <Route path='/' exact component={Main}/>
-              <Route path='/admin/products' component={Products}/>
+              <Route path='/admin/products' exact component={Products}/>
+              <Route path='/admin/products/create' exact component={ProductsCreate}/>
+              <Route path='/admin/products/:id/edit' exact component={ProductsEdit}/>
             </BrowserRouter>
             
           
