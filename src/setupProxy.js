a812b6a2319e65c7308b8ea233eebd436cf518c0
Joongseok Jeon <jeonjungseok1203@gmail.com>
2023-05-31 02:34:04 +0000

    [MOD] k8s manifest versioning

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,14 +3,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api/products', {
-      target: 'http://admin-svc-np.default.svc.cluster.local:8000',
+      target: 'http://admin-svc.default.svc.cluster.local:8000',
       changeOrigin: true,
     })
   );
 
   app.use(
     createProxyMiddleware('/api/product', {
-      target: 'http://main-svc-np.default.svc.cluster.local:5000',
+      target: 'http://main-svc.default.svc.cluster.local:5000',
       changeOrigin: true,
     })
   );
